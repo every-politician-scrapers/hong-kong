96314381ab856f685b707461d2b0cca390e61800
Tony Bowden <tony@tmtm.com>
2022-05-16 14:11:06 +0300

    compare members of the Legislative Council

---
+++ sources/legco/compare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bundle exec ruby scraper.rb $(jq -r .source meta.json) | qsv select item,itemLabel,startDate,endDate | qsv rename item,name,startDate,endDate > scraped.csv
+wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s name,startDate > wikidata.csv
+bundle exec ruby diff.rb | qsv sort -s name | tee diff.csv
+
+cd ~-
---
+++ sources/legco/scraper.rb
@@ -0,0 +1,42 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class ArrayDate < WikipediaDate
+  def to_s
+    date_str.map { |str| format('%02d', str) }.join('-')
+  end
+end
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    '任期'
+  end
+
+  def table_number
+    'position()<9'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name dob term dates].freeze
+    end
+
+    def combo_date
+      raw_combo_date.scan(/(\d+)年(\d+)月(\d+)日/)
+    end
+
+    def date_class
+      ArrayDate
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
